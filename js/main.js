3a48b4ea4a742a0cc07583067162d078e9a460df
tatojajo <115106101+tatojajo@users.noreply.github.com>
2022-11-08 20:20:17 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When scroll down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When click on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
